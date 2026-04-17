e6985eedd451a1c75acc309a1e3d8df40fe866a2
SXXian0504 <xsx0504@gmail.com>
2026-04-18 00:06:52 +0800

    init

---
+++ src/main/java/com/sxxian/marketingagent/MarketingAgentApplication.java
@@ -0,0 +1,13 @@
+package com.sxxian.marketingagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketingAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sxxian/marketingagent/MarketingAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxxian.marketingagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
