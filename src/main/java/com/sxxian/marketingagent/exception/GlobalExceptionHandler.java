f0ce4a27549af55e31852a7acafeb08df64c46ab
SXXian0504 <xsx0504@gmail.com>
2026-04-18 00:43:06 +0800

    chore: 初始化项目，增加了一些通用类

---
+++ src/main/java/com/sxxian/marketingagent/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.sxxian.marketingagent.exception;
+
+import com.sxxian.marketingagent.common.BaseResponse;
+import com.sxxian.marketingagent.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
+
